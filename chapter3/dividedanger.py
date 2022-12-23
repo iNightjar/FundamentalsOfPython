2422fb2bb94a10347db1117d23f118b89b853122
iNightjar <mohamedmedhateltlawy@gmail.com>
2022-12-23 05:49:15 +0200

    run-time error in dividing by zero

---
+++ chapter3/dividedanger.py
@@ -0,0 +1,10 @@
+# file dividedanger.py
+
+# get two integers from the user
+
+print('Please enter the two numbers to divide')
+dividend = int(input('Please enter the divident:'))
+divisor = int(input('Please enter the divisor:'))
+
+# divide them and report the result
+print(dividend, '/', divisor, '/', dividend/divisor)
