256a45dab52f75bbf8c329c663b9d52d05db6adc
iNightjar <mohamedmedhateltlawy@gmail.com>
2023-01-01 21:27:10 +0200

    usign multi-way conditional statement

---
+++ chapter4/multivseq1.py
@@ -0,0 +1,11 @@
+num = int(input("Enter a number: "))
+if num == 1 :
+    print("you entered one")
+elif num == 1:
+    print("you entered two")
+elif num > 5 :
+    print("you entered number graeter than 5. ")
+elif num == 7 : 
+    print("you entered seven")
+else:
+    print("you entered some other number")
