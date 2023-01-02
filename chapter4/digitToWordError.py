08c41d1dff77504acc28e990cfef2bf93ebfab5b
iNightjar <mohamedmedhateltlawy@gmail.com>
2023-01-02 08:22:43 +0200

    in range number, multi-way conditional statements

---
+++ chapter4/digitToWordError.py
@@ -0,0 +1,15 @@
+value = int(input("Please enter an integer in the range 0 ... 5: "))
+answer = "not in range"  # default answer
+if value == 0:
+    answer = "zero"
+elif value == 1:
+    answer = "one"
+elif value == 2:
+    answer = "two"
+elif value == 3:
+    amswer = "Three"
+elif value == 4:
+    answer = "four"
+elif value == 5:
+    answer = "five"
+print("The numer you entered was", answer)
