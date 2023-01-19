feb84df5a561ff5667fdcec76b0922072090350f
iNightjar <mohamedmedhateltlawy@gmail.com>
2023-01-19 23:56:13 +0200

    for iterator to range of number

---
+++ chapter7/ciountToTenfunc.py
@@ -0,0 +1,9 @@
+# count to ten and print each number on its own line
+
+def count():
+    for i in range(1, 11):
+        print(i ,end=' ')
+    print()
+
+print("Going to count to ten ... ")
+count()
