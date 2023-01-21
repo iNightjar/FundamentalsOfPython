14ed36e2ee2ee91ca70bc24038fafe214877d033
iNightjar <mohamedmedhateltlawy@gmail.com>
2023-01-21 03:00:04 +0200

    compute GCD for 2 numbers

---
+++ chapter7/docGCD.py
@@ -0,0 +1,12 @@
+def gcd(n1, n2):
+    """compute the greatest common divisor of integers n1 and n2"""
+    # determine the smaller of n1 and n2
+    min =  n1 if n1 < n2 else n2
+
+    # 1 definitely is a common factor to all ints
+    largset_factor =1
+    for i in range(1, min + 1):
+        if n1 % i == 0 and n2 % i == 0:
+            largest_factor = i  # found the greatest common divisor
+
+    return largest_factor
