da59492a6193518fb1ee9dc1057785997efe22d9
iNightjar <mohamedmedhateltlawy@gmail.com>
2023-02-15 05:28:43 +0200

    modes with TextIOWrapper

--- chapter9/simplefileread.py
+++ chapter9/simplefileread.py
@@ -1,4 +1,9 @@
-f = open('data.dat')
+f = open('data.txt')
 for line in f:
     print(line.strip())
 f.close()
+
+
+# 'r' opens the file for reading
+# 'w' opens the file for writing; creates a new file
+# 'a' opens the file to append data to it
