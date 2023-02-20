5bd3650d3accb86a799f674bc0bb9d350c6d0a18
iNightjar <mohamedmedhateltlawy@gmail.com>
2023-02-20 06:08:10 +0200

    list travirsal with range

---
+++ chapter10/heterolistforindex.py
@@ -0,0 +1,12 @@
+collection = [24.2, 4, 'word', print, 19, -0.03, 'end']
+
+for iteration in range(len(collection)):
+    print(collection[iteration])
+
+
+for item in collection:
+    print(item)
+
+
+
+print(len(collection))
