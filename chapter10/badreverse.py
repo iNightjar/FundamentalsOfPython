b3c51f400752943e1a63edd667d6d2a8b5be8470
iNightjar <mohamedmedhateltlawy@gmail.com>
2023-03-03 12:31:45 +0200

    bad list reverse

---
+++ chapter10/badreverse.py
@@ -0,0 +1,21 @@
+def make_list():
+    """
+    builds a list from input provided by the user
+    """
+
+    result = []
+    in_val = 0
+    while in_val >= 0:
+        in_val = int(input("Enter valid positive numbers only: "))
+        if in_val >= 0:
+            result = result + [in_val]
+    return result
+
+def main():
+    revers = make_list()
+    # print the list in reverse
+    for iterator in range(len(revers), -1, -1):
+        print(revers[iterator], end=' ')
+    print()
+
+main()
