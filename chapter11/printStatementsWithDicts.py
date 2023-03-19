976a718dd0c290638e90d0311424c49c2be05ab0
Muhamed Medhat <mohamedmedhateltlawy@gmail.com>
2023-03-19 08:12:05 +0200

    print statements with dictionaries

---
+++ chapter11/printStatementsWithDicts.py
@@ -0,0 +1,22 @@
+# # dictionary printing
+# dict = {'fred' : 44, 'ella' : 39, 'owen' : 40, 'zoe': 41}
+# for iterator in dict.keys():
+#     print(iterator, end=' ')
+
+# print()
+
+# dict = {'fred': 44, 'ella': 39, 'owen': 40, 'zoe': 41}
+# for iterator in dict:
+#     print(iterator, end=' ')
+# print()
+
+
+dict = {'fred': 44, 'ella': 39, 'owen': 40, 'zoe': 41}
+# for iterator in dict.values():
+    # print(iterator, end=' ')
+# print()
+
+
+for iterator, secondIterator in dict.items():
+    print( secondIterator)
+print()
