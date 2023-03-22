58dab6f1683c3350f4e4635fbdd233528d6b60bb
Muhamed Medhat <mohamedmedhateltlawy@gmail.com>
2023-03-22 06:33:47 +0200

    add more print statements with dict

--- chapter11/printStatementsWithDicts.py
+++ chapter11/printStatementsWithDicts.py
@@ -17,6 +17,10 @@ dict = {'fred': 44, 'ella': 39, 'owen': 40, 'zoe': 41}
 # print()
 
 
-for iterator, secondIterator in dict.items():
-    print( secondIterator)
-print()
+# for iterator, secondIterator in dict.items():
+#     print( secondIterator)
+# print()
+
+var = None
+if var:
+    print("hello world!")
