32a769f93762ba00720a24a575c833570f3b9503
Muhamed Medhat <mohamedmedhateltlawy@gmail.com>
2023-03-24 07:05:46 +0200

    print key:value pairs with dict

--- chapter11/printStatementsWithDicts.py
+++ chapter11/printStatementsWithDicts.py
@@ -12,15 +12,16 @@
 
 
 dict = {'fred': 44, 'ella': 39, 'owen': 40, 'zoe': 41}
-# for iterator in dict.values():
-    # print(iterator, end=' ')
-# print()
+for iterator, values in dict.items():
+    print(iterator, end=' ')
+    print(values, end=' ')
+print()
 
 
 # for iterator, secondIterator in dict.items():
 #     print( secondIterator)
 # print()
 
-var = None
-if var:
-    print("hello world!")
+# var = None
+# if var:
+#     print("hello world!")
