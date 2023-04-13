2dcb50f99ef42a10d23a03ec30219f7f02547b36
Muhamed Medhat <60796459+iNightjar@users.noreply.github.com>
2023-04-13 19:41:20 +0200

    Revert "Chapter12 source code finished"

--- chapter12/divideNumbers.py
+++
@@ -1,6 +0,0 @@
-# Get two integers from the user
-
-print("Please enter two numbers to divide. ")
-num1 = int(input("Please enter the divident. "))
-num2 = int(input("Please enter the divisor. "))
-print('{0} divided by {1} = {2}'.format(num1, num2, num1/num2))
--- chapter12/enterIntException.py
+++
@@ -1,5 +0,0 @@
-try:
-    val = int(input("Please enter a small positive integer: "))
-    print("You entered", val)
-except ValueError:
-    print("input not accepted")
