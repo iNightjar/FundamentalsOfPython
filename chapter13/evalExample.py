3bda4ffd394b2fd36385a525f62a20f8c4720142
Muhamed Medhat <mohamedmedhateltlawy@gmail.com>
2023-04-19 17:15:35 +0200

    eval built in function example

---
+++ chapter13/evalExample.py
@@ -0,0 +1,10 @@
+# input a string containing a python expression
+expression = "2 + 3 * 5"
+
+# evaluate the expression using eval()
+result = eval(expression)
+
+# print the result
+print(result)
+
+
