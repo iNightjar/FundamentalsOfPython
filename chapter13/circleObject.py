72bbc322bdd33e9dc00257ccda9164f2d6e57ae2
Muhamed Medhat <mohamedmedhateltlawy@gmail.com>
2023-04-28 07:20:56 +0300

    setting radius to private attribute

--- chapter13/circleObject.py
+++ chapter13/circleObject.py
@@ -6,11 +6,11 @@ class circle:
         if radius < 0:
             raise ValueError("Negative Radius.")
         self.center = center
-        self.radius = radius
+        self._radius = radius
 
     def get_radius(self):
         """ Return the radius of the circle. """
-        return self.radius
+        return self._radius
     def get_center(self):
         """ Return the coordinatess of the center"""
         return self.center
@@ -18,7 +18,7 @@ class circle:
     def get_area(self):
         """ Compute and return the circumference of the circle"""
         from math import pi
-        return 2*pi*self.radius
+        return 2*pi*self._radius
     
     def move(self, pt):
         """ Moves the enter of the circle to point pt"""
@@ -26,16 +26,14 @@ class circle:
 
     def grow(self):
         """ Increases the radius of the circle"""
-        self.radius += 1
+        self._radius += 1
     
     def shrink(self):
         """ Decreases the radius of the circle;
         does not affect a circle with radius zero"""
-        if self.radius > 0:
-            self.radius -= 1
+        if self._radius > 0:
+            self._radius -= 1
 
 
-sample = circle((10, 3.4), 5 )
-
-
-print(" circle radius is: ", sample.get_radius())
+# sample = circle((10, 3.4), 5 )
+# print(" circle radius is: ", sample.get_radius())
